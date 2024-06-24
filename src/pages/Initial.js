54dbbf21a2c8bbd683bcd30d75aad09a70adee6e
callbacked <alexanderjunior120@gmail.com>
2024-06-24 01:29:07 +0000

    slight changes

--- src/pages/Initial.js
+++ src/pages/Initial.js
@@ -9,9 +9,9 @@ const Initial = () => {
   const [isSearching, setIsSearching] = useState(false);
 
   return (
-    <div className="initial-page flex items-center justify-center min-h-screen bg-gray-900 p-5">
-      <div className="content-wrapper flex flex-col items-center w-full">
-        <div className="remote-and-carousel flex flex-col items-center w-full max-w-6xl">
+    <div className="initial-page flex items-center justify-center min-h-screen bg-gray-900">
+      <div className="content-wrapper flex flex-col items-center w-full h-full">
+        <div className="remote-and-carousel flex flex-col items-center w-full max-w-6xl p-4">
           <div className="relative w-full">
             <RemoteControl rokuIp={rokuIp} setRokuIp={setRokuIp} hideIpBar={isSearching} />
             <div className="absolute top-4 right-4">
